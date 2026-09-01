bf379871acd8d9b3d2b753864fd3875c575bf56c
Peter Steinberger <steipete@gmail.com>
2026-08-31 22:57:56 -0700

    fix(codex): preserve selected workspace in stacked refreshes

--- Sources/CodexBar/UsageStore+TokenAccounts.swift
+++ Sources/CodexBar/UsageStore+TokenAccounts.swift
@@ -428,7 +428,7 @@ extension UsageStore {
                         authFingerprint: authFingerprint ?? (requiresLiveAuth ? nil : account.authFingerprint),
                         workspaceAccountID: authFingerprint == nil && requiresLiveAuth
                             ? nil
-                            : (workspaceAccountID ?? account.workspaceAccountID)))
+                            : (account.workspaceAccountID ?? workspaceAccountID)))
             })
         let visibleAccounts = projection.visibleAccounts.map { account in
             guard case let .managedAccount(id) = account.selectionSource else { return account }
